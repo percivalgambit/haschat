4ccf951f206a4d0711a6b3ff0df8c14fff4de1a7
Lee Ehudin <percivalgambit@gmail.com>
2015-05-23 16:07:41 -0500

    set up initial threaded server to accept incomming connections

--- src/Haschat.hs
+++ src/Haschat.hs
@@ -5,14 +5,18 @@ module Haschat (haschat, defaultPort, chatServerPort) where
 import Control.Concurrent (forkIO)
 import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
 import System.Environment (lookupEnv)
-import System.IO (stderr, hSetBuffering, hPutStrLn, BufferMode(..))
+import System.IO (stderr, hSetBuffering, hPutStrLn, BufferMode(..), Handle)
 
 defaultPort :: Int
 defaultPort = 22311
 
 -- | Chat server entry point.
 haschat :: IO ()
-haschat = return ()
+haschat = withSocketsDo $ do
+    serverPort <- chatServerPort
+    listenSock <- listenOn $ PortNumber (fromIntegral serverPort)
+    hPutStrLn stderr $ "Listening on port " ++ (show serverPort)
+    serverLoop listenSock 1
 
 chatServerPort :: IO Int
 chatServerPort = do
@@ -32,3 +36,17 @@ chatServerPort = do
                 , "Using default port " ++ show defaultPort
                 ]
             return defaultPort
+
+serverLoop :: Socket -> Int -> IO ()
+serverLoop listenSock userId = do
+    (handle, hostname, clientPort) <- accept listenSock
+    hPutStrLn stderr $ unwords
+        [ "Accepted connection from"
+        , hostname ++ ":" ++ (show clientPort)
+        ]
+    hSetBuffering handle NoBuffering
+    _ <- forkIO $ processChat handle userId
+    serverLoop listenSock (userId + 1)
+
+processChat :: Handle -> Int -> IO ()
+processChat handle userId = return ()
